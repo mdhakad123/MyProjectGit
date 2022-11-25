de7f7738c2e9ee1931e9c6848257987ff13c1e25
MONU12 <mdhakad2015@gmail.com>
2022-11-25 18:58:49 +0530

    branchX

---
+++ src/practice/Jupiter2.java
@@ -0,0 +1,41 @@
+package practice;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class Jupiter2 {
+
+	public static void main(String[] args)
+	{
+		  String st ="MY NAME IS MONU DHAKAD";
+		  st=st.replaceAll("\\s","");
+		  HashMap<Character, Integer>map = new HashMap<Character,Integer>();
+		  int count=0;
+		  for(int i=0;i<st.length();i++)
+		  {
+			  count=0;
+			  for(int j=0;j<st.length();j++)
+			  {
+				  if(st.charAt(i)==st.charAt(j))
+				  {
+					  count++;
+				  }
+			  }
+			  map.put(st.charAt(i),count);
+			  
+		  }
+		  System.out.println(map);
+		  
+		  Iterator<Character>itr=map.keySet().iterator();
+		  while(itr.hasNext())
+		  {
+			  Character temp=itr.next();
+			  if(map.get(temp)>1)
+			  {
+				  System.out.println(temp+"  "+map.get(temp));
+			  }
+		  }
+		  
+	}
+
+}
